1e9003c502821c1608ab2a5eeccf1e43b9f3e607
arifromdon <arif@clappingape.com>
2018-11-10 23:17:33 +0700

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
+  resources :posts
   root 'blogs#index'
+  root 'posts#index'
   get 'blogs/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
